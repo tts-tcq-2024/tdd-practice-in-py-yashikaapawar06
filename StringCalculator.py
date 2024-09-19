b56b676f99f767a8633c02188b2b6807fd4ed4c2
yashikaapawar06 <94790095+yashikaapawar06@users.noreply.github.com>
2024-09-19 13:25:23 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,3 +1,4 @@
+import re
 def add(numbers: str) -> int:
     return 0
 
@@ -25,5 +26,13 @@ def add(numbers: str) -> int:
     num_list = filter_large_numbers(num_list)
     return sum(num_list)
 
-
+def add(numbers: str) -> int:
+    if numbers == "":
+        return 0
+    if numbers.startswith("//"):
+        delimiter, numbers = re.split(r"\n", numbers[2:], maxsplit=1)
+        numbers = numbers.replace(delimiter, ",")
+    num_list = list(map(int, numbers.split(",")))
+    num_list = [num for num in num_list if num <= 1000]
+    return sum(num_list)
 
