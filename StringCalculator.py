7ecc70f5b4c465ef87d6023f81e8db5908ea7339
yashikaapawar06 <94790095+yashikaapawar06@users.noreply.github.com>
2024-09-19 13:17:53 +0530

    Update StringCalculator.py

--- StringCalculator.py
+++ StringCalculator.py
@@ -6,3 +6,9 @@ def add(numbers: str) -> int:
         return 0
     return int(numbers)
 
+def add(numbers: str) -> int:
+    if numbers == "":
+        return 0
+    num_list = list(map(int, numbers.split(",")))
+    return sum(num_list)
+
